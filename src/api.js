007f66b02069f8c5e68b4c3b3a49816a03c98891
Khalil <khalilnasri92@gmail.com>
2021-12-14 13:36:49 +0100

    added api function to send and get data

---
+++ src/api.js
@@ -0,0 +1,32 @@
+const newGame = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'Codm'
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
+};
+
+const sendScore = (user, score) => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/eQUcQ5gi4JK5SLh8JJKd/scores/', {
+  method: 'POST',
+  body: JSON.stringify({
+    "user": user,
+    "score": score
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
+};
+
+const getScore = async () => {
+  const data = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/eQUcQ5gi4JK5SLh8JJKd/scores/')
+  .then(res => res.json());
+  return data;
+}
+
+module.exports = { newGame, sendScore, getScore }
